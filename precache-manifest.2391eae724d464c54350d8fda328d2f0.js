b4f05d8415f2eea0326c8923dc00151091717a4d
Brandon <brandon.david.spicer@gmail.com>
2020-10-26 15:08:31 -0700

    Updates

---
+++ precache-manifest.2391eae724d464c54350d8fda328d2f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe2d1209b91ee45b92345cfdaf5db20",
+    "url": "/ReactECA/index.html"
+  },
+  {
+    "revision": "e1a8fb1578557d90e728",
+    "url": "/ReactECA/static/css/main.8dd5370e.chunk.css"
+  },
+  {
+    "revision": "b5dcc8b6b623075efd4c",
+    "url": "/ReactECA/static/js/2.7620628f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactECA/static/js/2.7620628f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a8fb1578557d90e728",
+    "url": "/ReactECA/static/js/main.86c7c29d.chunk.js"
+  },
+  {
+    "revision": "64f30b96e511a7ca576b",
+    "url": "/ReactECA/static/js/runtime-main.08943834.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/testRepo/precache-manifest.e06a0bf5602162967a2985270bb59128.js"
+  "/ReactECA/precache-manifest.2391eae724d464c54350d8fda328d2f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/testRepo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactECA/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
